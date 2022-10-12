ba32fe70307aec43cff1a6a246ade0fe18498360
sergey <grani2010@yandex.ru>
2022-10-12 14:27:48 +0300

    lesson 4

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿/*Console.WriteLine("введите  число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите степень");
+int n =Convert.ToInt32(Console.ReadLine());
+int num_n=1;
+for (int i=0;i<n;i++)
+  {
+    num_n*=num;
+  }
+Console.WriteLine("{2}",num,n,num_n);
+
+
+
+
+
+
+
+
+Console.WriteLine("введите  число");
+int num = Convert.ToInt32(Console.ReadLine());
+int num_sum=0;
+ while(num>0)
+{
+    num_sum=num_sum+num%10;
+    num= num/10;
+}
+Console.WriteLine(num_sum);
+
+
+int []array = new int [8];
+for(int i=0;i<array.Length;i++)
+ array[i]=new Random().Next(0,9);
+ Console.WriteLine($"[{string.Join(",",array)}]");
+
+*/
